01a635db68575827048712c59bef3703b9c00141
JeanPierre-cmd <83962382+JeanPierre-cmd@users.noreply.github.com>
2025-09-05 09:18:34 -0400

    Updated package-lock.json

---
+++ src/features/viewer3d/pages/LocalViewer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ModelsPage from '../../../components/ModelViewer/ModelsPage';
+
+const LocalViewer: React.FC = () => {
+  return <ModelsPage />;
+};
+
+export default LocalViewer;
